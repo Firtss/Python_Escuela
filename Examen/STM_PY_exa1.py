476f21a632cf07581500121f181a10477f8e5b4f
Martin Santos <142352237+Firtss@users.noreply.github.com>
2023-12-01 17:03:23 -0800

    Update STM_PY_exa1.py

--- Examen/STM_PY_exa1.py
+++ Examen/STM_PY_exa1.py
@@ -10,3 +10,28 @@ def factorial(num):
 fact = factorial(num)
 print(fact)
 #https://github.com/Firtss/Python_Escuela/tree/main/Examen
+Altura = int(input("Dame la altura del triangulo: "))
+Base = int(input("Dame la base del triangulo: "))
+def area(Altura,Base):
+    area = Base * Altura / 2.0
+    return area
+
+area_triangulo = area(Altura,Base)
+print(area_triangulo)
+#https://github.com/Firtss/Python_Escuela/tree/main/Examen
+valido = False
+
+def validar(valor):
+    entero = int(valor)
+    return entero >= 0 and entero <= 100
+try:
+    print("Introduzca un valor: ",end="")
+    while not valido:
+        valor = input()
+        valido = validar(valor)
+        if not valido:
+            print("El valor no es valido")
+    print(f"El valor es valido {valor}")
+except: 
+    print("No introduciste un valor numerico")
+#https://github.com/Firtss/Python_Escuela/tree/main/Examen
